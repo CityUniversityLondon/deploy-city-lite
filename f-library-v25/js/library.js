0bb2b250911e7042506c86cd838cd6cabb245983
CUL Web Team <ucs-webteam@city.ac.uk>
2025-01-21 16:59:56 +0000

    01/21/25 16:59:54 | added missing plugin

---
+++ f-library-v25/js/library.js
@@ -0,0 +1,513 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/patterns-library.js":
+/*!*********************************!*\
+  !*** ./src/patterns-library.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _patterns_accordion_library_accordion_library__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./patterns/accordion/library/accordion-library */ "./src/patterns/accordion/library/accordion-library.js");
+
+
+/**
+ * Pattern library - City Library
+ *
+ * Exports an array of patterns to execute on startup.
+ *
+ * @module patterns
+ * @author Web Development
+ * @copyright City, University of London 2025
+ */
+// accordion - CityR
+// tabs - CityR
+// back-to-top web2020
+// opening-times 
+// primary nav - mobile
+
+/* harmony default export */ __webpack_exports__["default"] = ([_patterns_accordion_library_accordion_library__WEBPACK_IMPORTED_MODULE_0__["default"]]);
+
+/***/ }),
+
+/***/ "./src/patterns/accordion/library/accordion-library.js":
+/*!*************************************************************!*\
+  !*** ./src/patterns/accordion/library/accordion-library.js ***!
+  \*************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+
+
+/**
+ * modal
+ *
+ * @module patterns/accordion/accordion-library
+ * @author Web Development
+ * @copyright City, University of London 2025
+ *
+ */
+const className = 'accordion';
+console.log('foo');
+/* harmony default export */ __webpack_exports__["default"] = ({
+  launchFn: launchAccordion,
+  launchQuery: `.${className}`
+});
+
+/***/ }),
+
+/***/ "./src/patterns/devcorate/devcorate.js":
+/*!*********************************************!*\
+  !*** ./src/patterns/devcorate/devcorate.js ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "devcorate": () => (/* binding */ devcorate)
+/* harmony export */ });
+/* harmony import */ var _util__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../util */ "./src/util.js");
+
+
+/**
+ * Devcorate
+ *
+ * @module patterns/devcorate/devcorate
+ * @author Web Development
+ * @copyright City, University of London 2018
+ */
+
+/**
+ * Persist a query parameter to all same-site anchors inside an element.
+ *
+ * @param {HTMLElement} elem - An element containing anchors to decorate.
+ * @param {string} param - The name of a query parameter.
+ * @param {string} value - The value for the query parameter.
+ */
+
+function devcorate(elem, param, value) {
+  Array.from(elem.querySelectorAll('a')).forEach(anchor => {
+    if (anchor.origin === window.location.origin) {
+      const parameters = anchor.search ? (0,_util__WEBPACK_IMPORTED_MODULE_0__.parametersToObject)(anchor.search) : {};
+      parameters[param] = value;
+      anchor.href = `${anchor.origin}${anchor.pathname}${(0,_util__WEBPACK_IMPORTED_MODULE_0__.objectToParameters)(parameters)}${anchor.hash}`;
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "./src/util.js":
+/*!*********************!*\
+  !*** ./src/util.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "removeClass": () => (/* binding */ removeClass),
+/* harmony export */   "toArray": () => (/* binding */ toArray),
+/* harmony export */   "parametersToObject": () => (/* binding */ parametersToObject),
+/* harmony export */   "objectToParameters": () => (/* binding */ objectToParameters),
+/* harmony export */   "gaEvent": () => (/* binding */ gaEvent),
+/* harmony export */   "detectIE": () => (/* binding */ detectIE),
+/* harmony export */   "checkIntersectionObserver": () => (/* binding */ checkIntersectionObserver),
+/* harmony export */   "createHTMLElement": () => (/* binding */ createHTMLElement),
+/* harmony export */   "appendAll": () => (/* binding */ appendAll),
+/* harmony export */   "reduceMotion": () => (/* binding */ reduceMotion),
+/* harmony export */   "toBool": () => (/* binding */ toBool),
+/* harmony export */   "verticallyInWindow": () => (/* binding */ verticallyInWindow),
+/* harmony export */   "screenWidth": () => (/* binding */ screenWidth)
+/* harmony export */ });
+
+
+const partialMatchFn = m => s => s.indexOf(m) < 0,
+      fullMatchFn = m => s => s !== m;
+/**
+ * Remove a class name from an HTML element.
+ *
+ * @param {HTMLElement} elem - An HTML element.
+ * @param {string} className - A string representing the class to remove.
+ * @param {boolean} [removePartials] - If true, remove partially matching classes.
+ */
+
+
+function removeClass(elem, className, removePartials) {
+  const matchFn = removePartials ? partialMatchFn(className) : fullMatchFn(className);
+  elem.className = elem.className.split(/\s+/).filter(s => matchFn(s)).join(' ');
+}
+/**
+ *
+ * Turn DOM elements to array e.g return [li, li, li] from DOM element selecting 'li' <ul><li></li><li></li><li></li></ul>
+ *
+ * @param {object} object - Object
+ */
+
+function toArray(obj) {
+  let array = []; // iterate backwards ensuring that length is an UInt32
+
+  for (let i = obj.length >>> 0; i--;) {
+    array[i] = obj[i];
+  }
+
+  return array;
+}
+/**
+ * Turn a query string into an object.
+ *
+ * @param {string} parameterString - An HTML query string.
+ * @returns {object} An object containing the parameters.
+ */
+
+function parametersToObject(parameterString) {
+  const parameters = {};
+  parameterString.substr(1).split('&').forEach(parameter => {
+    const [k, v] = parameter.split('=');
+    parameters[k] = v;
+  });
+  return parameters;
+}
+/**
+* Turn an object into a query string.
+*
+* @param {object} parameterObj - An object containing the parameters and their values.
+* @returns {string} A string of the parameters.
+*/
+
+function objectToParameters(parameterObj) {
+  if (Object.keys(parameterObj).length > 0) {
+    let parameters = '?';
+
+    for (const key in parameterObj) {
+      parameters.length !== 1 && (parameters += '&');
+      parameters += key + '=' + parameterObj[key];
+    }
+
+    return parameters;
+  } else {
+    return '';
+  }
+}
+/**
+ * Track a Google Analytics event.
+ *
+ * @param {string} event - The event key for Tag Manager.
+ * @param {string} eventCategory - The event category.
+ * @param {string} eventAction - The event action.
+ * @param {string} [eventLabel] - The event label.
+ * @param {number} [eventValue] - The event value.
+ * @param {boolean} [nonInteractionHit] - Does this count as interacting with the page?
+ */
+
+function gaEvent(event, eventCategory, eventAction, eventLabel, eventValue, nonInteractionHit) {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: event,
+    eventCategory: eventCategory,
+    eventAction: eventAction,
+    eventLabel: eventLabel,
+    eventValue: eventValue,
+    nonInteractionHit: nonInteractionHit
+  });
+}
+function detectIE() {
+  const ua = window.navigator.userAgent,
+        versionLength = 10,
+        msie = ua.indexOf('MSIE '),
+        msieVersionOffset = 5;
+
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + msieVersionOffset, ua.indexOf('.', msie)), versionLength);
+  }
+
+  const trident = ua.indexOf('Trident/'),
+        tridentVersionOffset = 3;
+
+  if (trident > 0) {
+    // IE 11 => return version number
+    let rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + tridentVersionOffset, ua.indexOf('.', rv)), versionLength);
+  }
+
+  const edge = ua.indexOf('Edge/'),
+        edgeVersionOffset = 5;
+
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + edgeVersionOffset, ua.indexOf('.', edge)), versionLength);
+  } // other browser
+
+
+  return false;
+}
+/**
+ * Check if browser supports IntersectionObserver API
+ *
+ * @returns {Boolean} -
+ */
+
+function checkIntersectionObserver() {
+  if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+    return true;
+  } else {
+    return false;
+  }
+}
+/**
+ * Create elements containing classes, content and any other attributes
+ *
+ * @param {string} type - Type of element to create, e.g. "div", "button".
+ * @param {array} attributes - Array of objects specifying which attributes to assign to the element i.e. {label : "class", val : "container"}
+ */
+
+function createHTMLElement(type, attributes) {
+  let el = document.createElement(type);
+  attributes.map(function (att) {
+    att.label === 'content' ? el.appendChild(document.createTextNode(att.val)) : att.label === 'html' ? el.innerHTML = att.val : el.setAttribute(att.label, att.val);
+  });
+  return el;
+}
+/**
+ * Append an array of elements to an element.
+ *
+ * @param {HTMLElement} elem - The parent element.
+ * @param {HTMLElement[]} children - An array of elements to append to it.
+ */
+
+function appendAll(elem, children) {
+  children.forEach(child => elem.appendChild(child));
+}
+/**
+ * Detect whether reduced motion is enabled.
+ *
+ * If it is, you should shortcut animations, disable autoplay elements, avoid
+ * smooth scrolling in favour of jump scrolling, etc.
+ *
+ * Currently unsupported by Chrome, but works in Safari and Firefox.
+ *
+ * @returns {boolean} Boolean indicating that the reduce motion media query matches.
+ */
+
+function reduceMotion() {
+  const reducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+  return reducedMotionQuery.matches ? true : false;
+}
+/**
+ * Turns string boolean into real boolean.
+ *
+ * @param {string} s - A string that may be 'true'.
+ * @returns {boolean} True if 'true'.
+ */
+
+function toBool(s) {
+  return s === 'true' ? true : false;
+}
+/**
+ * Predicate testing whether an element is positioned in the window.
+ *
+ *
+ * @param {HTMLElement} elem - An HTML element.
+ * @returns {boolean} - Is it onscreen?
+ */
+
+function verticallyInWindow(elem) {
+  return elem.getBoundingClientRect().top >= 0 && elem.getBoundingClientRect().top <= window.innerHeight ? true : false;
+}
+/**
+ * Screen width
+ *
+ * @param {size} string - variable name for sreensize value. To be consistent with values stored in _variables.scss
+ */
+
+function screenWidth(size) {
+  switch (size) {
+    case 'tiny':
+      return 375;
+      break;
+
+    case 'mobile':
+      return 432;
+      break;
+
+    case 'tablet':
+      return 768;
+      break;
+
+    case 'between':
+      return 900;
+      break;
+
+    case 'small':
+      return 1024;
+      break;
+
+    case 'desktop':
+      return 1280;
+      break;
+
+    case 'large':
+      return 1440;
+      break;
+
+    case '1080':
+      return 1920;
+      break;
+
+    case '4k':
+      return 3840;
+      break;
+
+    case '8k':
+      return 7680;
+      break;
+
+    default:
+      return 1280;
+      break;
+  }
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/library.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "tryCatch": () => (/* binding */ tryCatch)
+/* harmony export */ });
+/* harmony import */ var _patterns_library__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./patterns-library */ "./src/patterns-library.js");
+/* harmony import */ var _util__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./util */ "./src/util.js");
+/* harmony import */ var _patterns_devcorate_devcorate__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./patterns/devcorate/devcorate */ "./src/patterns/devcorate/devcorate.js");
+
+
+/**
+ * Main entry - Library
+ *
+ * @author Web Development
+ * @copyright City, University of London 2025
+ */
+
+
+
+const edgeVersion = 12;
+/**
+ * try a function, log the error if it throws an exception.
+ *
+ * @param {Function} f - The function to call.
+ */
+
+function tryCatch(f) {
+  try {
+    return f();
+  } catch (e) {
+    (0,_util__WEBPACK_IMPORTED_MODULE_1__.gaEvent)('jsError', 'JavaScript error', `Line ${e.lineNumber} – ${e.message}`, `Pattern launch ${e.fileName} (${window.location})`, true);
+  }
+}
+/**
+ * Runs the function for each pattern against every element that has the
+ * matching class, unless the element also has that class with a '-no-js' suffix
+ * (for cases where you want to use the CSS for a pattern, but not the
+ * functionality).
+ *
+ * @param {object} pattern - An object containing the class name to trigger the pattern and the launch function to run for those elements.
+ */
+
+function launchPattern(pattern) {
+  if (typeof pattern === 'function') {
+    tryCatch(pattern);
+  } else if (pattern.launchQuery) {
+    const {
+      launchFn,
+      launchQuery
+    } = pattern;
+    Array.from(document.querySelectorAll(launchQuery)).filter(elem => elem.className.indexOf(`${launchQuery}-no-js`)).forEach(elem => tryCatch(() => launchFn(elem)));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (/(Trident|MSIE)/.test(navigator.userAgent)) {
+    return;
+  } else {
+    Array.from(document.getElementsByTagName('html')).forEach(html => {
+      let ie = (0,_util__WEBPACK_IMPORTED_MODULE_1__.detectIE)();
+      (0,_util__WEBPACK_IMPORTED_MODULE_1__.removeClass)(html, 'no-js', false);
+
+      if (ie >= edgeVersion) {
+        html.className = (html.className + ' js ' + 'edge').trim();
+      } else {
+        html.className = (html.className + ' js').trim();
+      }
+    });
+    _patterns_library__WEBPACK_IMPORTED_MODULE_0__["default"].forEach(launchPattern);
+    const parameters = (0,_util__WEBPACK_IMPORTED_MODULE_1__.parametersToObject)(location.search);
+    parameters['dev'] && (0,_patterns_devcorate_devcorate__WEBPACK_IMPORTED_MODULE_2__.devcorate)(document.querySelector('body'), 'dev', parameters['dev']);
+    parameters['rel'] && (0,_patterns_devcorate_devcorate__WEBPACK_IMPORTED_MODULE_2__.devcorate)(document.querySelector('body'), 'rel', parameters['rel']);
+  }
+}, false);
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=library.js.map
